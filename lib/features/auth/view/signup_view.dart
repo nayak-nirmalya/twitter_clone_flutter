84eea94fe04ec19bb60d604f7aadedb29be5c58d
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-02 22:23:56 +0530

    added sign up view

--- lib/features/auth/view/signup_view.dart
+++ lib/features/auth/view/signup_view.dart
@@ -5,14 +5,14 @@ import 'package:twitter_clone/constants/constants.dart';
 import 'package:twitter_clone/common/rounded_small_button.dart';
 import 'package:twitter_clone/features/auth/widgets/auth_field.dart';
 
-class LoginView extends StatefulWidget {
-  const LoginView({super.key});
+class SignUpView extends StatefulWidget {
+  const SignUpView({super.key});
 
   @override
-  State<LoginView> createState() => _LoginViewState();
+  State<SignUpView> createState() => _SignUpViewState();
 }
 
-class _LoginViewState extends State<LoginView> {
+class _SignUpViewState extends State<SignUpView> {
   final appbar = UIConstants.appBar();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
