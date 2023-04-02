3e6b79b79a257de9c149a5583b6375be3ec2fe79
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-02 18:45:49 +0530

    added appwrite constants

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = "64297f926fe94e928c3a";
+  static const String projectId = "642911e0eebb79e25ce2";
+  static const String endPoint = "http://localhost:80/v1";
+}
