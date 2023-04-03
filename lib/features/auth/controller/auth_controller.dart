3cecb204afca7722b382a8f63c294706948a2035
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-03 10:33:59 +0530

    added utils fun

---
+++ lib/features/auth/controller/auth_controller.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:twitter_clone/apis/auth_api.dart';
+import 'package:twitter_clone/core/utils.dart';
+
+class AuthController extends StateNotifier<bool> {
+  final AuthAPI _authAPI;
+  AuthController({required AuthAPI authAPI})
+      : _authAPI = authAPI,
+        super(false);
+
+  void signUp({
+    required String email,
+    required String password,
+    required BuildContext context,
+  }) async {
+    state = true;
+    final res = await _authAPI.signUp(email: email, password: password);
+
+    res.fold((l) => showSnackBar(context, l.message), (r) => print(r.name));
+  }
+}
