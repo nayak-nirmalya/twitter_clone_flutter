74b2c8a4144042d95bcb7cc3748118414854dda2
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-04 13:45:27 +0530

    changed address from localhost to ip address

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -1,7 +1,8 @@
 class AppwriteConstants {
   static const String databaseId = "64297f926fe94e928c3a";
   static const String projectId = "642911e0eebb79e25ce2";
-  static const String endPoint = "http://localhost:80/v1";
+  static const String endPoint = "http://192.168.140.224:80/v1";
+  // static const String endPoint = "http://localhost:80/v1";
 
   static const String usersCollection = "642bcd918108cf817bf8";
 }
