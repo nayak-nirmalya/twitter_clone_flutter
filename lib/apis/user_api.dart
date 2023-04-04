6816e1d821c5ce0ed258f1c0a51b37ce45597c2b
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-04 12:46:24 +0530

    added user collection id

--- lib/apis/user_api.dart
+++ lib/apis/user_api.dart
@@ -1,13 +1,42 @@
+import 'package:appwrite/appwrite.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:twitter_clone/constants/appwrite_constants.dart';
 import 'package:twitter_clone/core/core.dart';
+import 'package:twitter_clone/models/user_model.dart';
 
 abstract class IUserAPI {
-  FutureEitherVoid saveUserData();
+  FutureEitherVoid saveUserData(UserModel userModel);
 }
 
 class UserAPI implements IUserAPI {
+  final Databases _db;
+  UserAPI({required Databases db}) : _db = db;
+
   @override
-  FutureEitherVoid saveUserData() {
-    // TODO: implement saveUserData
-    throw UnimplementedError();
+  FutureEitherVoid saveUserData(UserModel userModel) async {
+    try {
+      await _db.createDocument(
+        databaseId: AppwriteConstants.databaseId,
+        collectionId: AppwriteConstants.usersCollection,
+        documentId: ID.unique(),
+        data: userModel.toMap(),
+      );
+
+      return right(null);
+    } on AppwriteException catch (e, st) {
+      return left(
+        Failure(
+          e.message ?? 'Unexpected Error Occured!',
+          st,
+        ),
+      );
+    } catch (e, st) {
+      return left(
+        Failure(
+          e.toString(),
+          st,
+        ),
+      );
+    }
   }
 }
--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -2,4 +2,6 @@ class AppwriteConstants {
   static const String databaseId = "64297f926fe94e928c3a";
   static const String projectId = "642911e0eebb79e25ce2";
   static const String endPoint = "http://localhost:80/v1";
+
+  static const String usersCollection = "642bcd918108cf817bf8";
 }
