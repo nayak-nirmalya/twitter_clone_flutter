ee441cb136105a6bac2fb2d1fb9ad53d20cb0f86
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-05 10:10:44 +0530

    completed model and enum

--- lib/core/enums/tweet_type_enum.dart
+++ lib/core/enums/tweet_type_enum.dart
@@ -5,3 +5,16 @@ enum TweetType {
   final String type;
   const TweetType(this.type);
 }
+
+extension ConvertTweet on String {
+  TweetType toTweetTypeEnum() {
+    switch (this) {
+      case 'text':
+        return TweetType.text;
+      case 'image':
+        return TweetType.image;
+      default:
+        return TweetType.text;
+    }
+  }
+}
--- lib/models/tweet_model.dart
+++ lib/models/tweet_model.dart
@@ -1,8 +1,131 @@
+import 'package:flutter/foundation.dart';
+
+import 'package:twitter_clone/core/enums/tweet_type_enum.dart';
+
+@immutable
 class Tweet {
+  final String id;
   final String text;
   final List<String> hashTags;
   final String link;
   final List<String> imageLinks;
   final String uid;
-  final 
+  final TweetType tweetType;
+  final DateTime tweetedAt;
+  final List<String> likes;
+  final List<String> commentIds;
+  final int reShareCount;
+  const Tweet({
+    required this.id,
+    required this.text,
+    required this.hashTags,
+    required this.link,
+    required this.imageLinks,
+    required this.uid,
+    required this.tweetType,
+    required this.tweetedAt,
+    required this.likes,
+    required this.commentIds,
+    required this.reShareCount,
+  });
+
+  Tweet copyWith({
+    String? id,
+    String? text,
+    List<String>? hashTags,
+    String? link,
+    List<String>? imageLinks,
+    String? uid,
+    TweetType? tweetType,
+    DateTime? tweetedAt,
+    List<String>? likes,
+    List<String>? commentIds,
+    int? reShareCount,
+  }) {
+    return Tweet(
+      id: id ?? this.id,
+      text: text ?? this.text,
+      hashTags: hashTags ?? this.hashTags,
+      link: link ?? this.link,
+      imageLinks: imageLinks ?? this.imageLinks,
+      uid: uid ?? this.uid,
+      tweetType: tweetType ?? this.tweetType,
+      tweetedAt: tweetedAt ?? this.tweetedAt,
+      likes: likes ?? this.likes,
+      commentIds: commentIds ?? this.commentIds,
+      reShareCount: reShareCount ?? this.reShareCount,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    final result = <String, dynamic>{};
+
+    result.addAll({'id': id});
+    result.addAll({'text': text});
+    result.addAll({'hashTags': hashTags});
+    result.addAll({'link': link});
+    result.addAll({'imageLinks': imageLinks});
+    result.addAll({'uid': uid});
+    result.addAll({'tweetType': tweetType.type});
+    result.addAll({'tweetedAt': tweetedAt.millisecondsSinceEpoch});
+    result.addAll({'likes': likes});
+    result.addAll({'commentIds': commentIds});
+    result.addAll({'reShareCount': reShareCount});
+
+    return result;
+  }
+
+  factory Tweet.fromMap(Map<String, dynamic> map) {
+    return Tweet(
+      id: map['id'] ?? '',
+      text: map['text'] ?? '',
+      hashTags: List<String>.from(map['hashTags']),
+      link: map['link'] ?? '',
+      imageLinks: List<String>.from(map['imageLinks']),
+      uid: map['uid'] ?? '',
+      tweetType: (map['tweetType'] as String).toTweetTypeEnum(),
+      tweetedAt: DateTime.fromMillisecondsSinceEpoch(map['tweetedAt']),
+      likes: List<String>.from(map['likes']),
+      commentIds: List<String>.from(map['commentIds']),
+      reShareCount: map['reShareCount']?.toInt() ?? 0,
+    );
+  }
+
+  @override
+  String toString() {
+    return 'Tweet(id: $id, text: $text, hashTags: $hashTags, link: $link, imageLinks: $imageLinks, uid: $uid, tweetType: $tweetType, tweetedAt: $tweetedAt, likes: $likes, commentIds: $commentIds, reShareCount: $reShareCount)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Tweet &&
+        other.id == id &&
+        other.text == text &&
+        listEquals(other.hashTags, hashTags) &&
+        other.link == link &&
+        listEquals(other.imageLinks, imageLinks) &&
+        other.uid == uid &&
+        other.tweetType == tweetType &&
+        other.tweetedAt == tweetedAt &&
+        listEquals(other.likes, likes) &&
+        listEquals(other.commentIds, commentIds) &&
+        other.reShareCount == reShareCount;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        text.hashCode ^
+        hashTags.hashCode ^
+        link.hashCode ^
+        imageLinks.hashCode ^
+        uid.hashCode ^
+        tweetType.hashCode ^
+        tweetedAt.hashCode ^
+        likes.hashCode ^
+        commentIds.hashCode ^
+        reShareCount.hashCode;
+  }
 }
