105c6f5b4ec477cf02a209a6e7a18c28e524fe0d
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-05 19:11:04 +0530

    added tweet btn icons

--- lib/features/tweet/widgets/tweet_card.dart
+++ lib/features/tweet/widgets/tweet_card.dart
@@ -1,10 +1,13 @@
+import 'package:any_link_preview/any_link_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twitter_clone/common/common.dart';
+import 'package:twitter_clone/constants/assets_constants.dart';
 import 'package:twitter_clone/core/enums/tweet_type_enum.dart';
 import 'package:twitter_clone/features/auth/controller/auth_controller.dart';
 import 'package:twitter_clone/features/tweet/widgets/carousel_image.dart';
 import 'package:twitter_clone/features/tweet/widgets/hashtag_text.dart';
+import 'package:twitter_clone/features/tweet/widgets/tweet_icon_button.dart';
 import 'package:twitter_clone/models/tweet_model.dart';
 import 'package:twitter_clone/theme/pallete.dart';
 import 'package:timeago/timeago.dart' as timeago;
@@ -63,17 +66,67 @@ class TweetCard extends ConsumerWidget {
                             text: tweet.text,
                           ),
                           if (tweet.tweetType == TweetType.image)
-                            CarouselImage(imageLinks: tweet.imageLinks)
-                          
+                            CarouselImage(imageLinks: tweet.imageLinks),
+
                           if (tweet.link.isNotEmpty) ...[
-                            const SizedBox(height: 4,),
-                            
-                          ]
+                            const SizedBox(
+                              height: 4,
+                            ),
+                            AnyLinkPreview(
+                              displayDirection:
+                                  UIDirection.uiDirectionHorizontal,
+                              link: 'https://${tweet.link}',
+                            ),
+                          ],
+                          Container(
+                            margin: const EdgeInsets.only(
+                              top: 10,
+                              right: 20,
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                TweetIconButton(
+                                  pathName: AssetsConstants.viewsIcon,
+                                  text: (tweet.commentIds.length +
+                                          tweet.reShareCount +
+                                          tweet.likes.length)
+                                      .toString(),
+                                  onTap: () {},
+                                ),
+                                TweetIconButton(
+                                  pathName: AssetsConstants.commentIcon,
+                                  text: tweet.commentIds.length.toString(),
+                                  onTap: () {},
+                                ),
+                                TweetIconButton(
+                                  pathName: AssetsConstants.retweetIcon,
+                                  text: tweet.reShareCount.toString(),
+                                  onTap: () {},
+                                ),
+                                TweetIconButton(
+                                  pathName: AssetsConstants.likeOutlinedIcon,
+                                  text: tweet.likes.length.toString(),
+                                  onTap: () {},
+                                ),
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(
+                                    Icons.share_outlined,
+                                    size: 25,
+                                    color: Pallete.greyColor,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(height: 1),
                         ],
                       ),
                     ),
                   ],
                 ),
+                const Divider(color: Pallete.greyColor),
               ],
             );
           },
