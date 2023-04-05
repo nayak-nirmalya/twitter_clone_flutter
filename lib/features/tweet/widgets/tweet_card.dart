7bd73d241d33e88e0885f19e9a6ccb5f3e8dae7c
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-05 18:25:15 +0530

    added styling to links and hashtags

--- lib/features/tweet/widgets/tweet_card.dart
+++ lib/features/tweet/widgets/tweet_card.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twitter_clone/common/common.dart';
 import 'package:twitter_clone/features/auth/controller/auth_controller.dart';
+import 'package:twitter_clone/features/tweet/widgets/hashtag_text.dart';
 import 'package:twitter_clone/models/tweet_model.dart';
 import 'package:twitter_clone/theme/pallete.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 class TweetCard extends ConsumerWidget {
   final Tweet tweet;
@@ -24,31 +26,41 @@ class TweetCard extends ConsumerWidget {
                         radius: 35,
                       ),
                     ),
-                    Column(
-                      children: [
-                        // retweeted
-                        Row(
-                          children: [
-                            Container(
-                              margin: const EdgeInsets.only(right: 5),
-                              child: Text(
-                                user.name,
-                                style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 19,
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          // retweeted
+                          Row(
+                            children: [
+                              Container(
+                                margin: const EdgeInsets.only(right: 5),
+                                child: Text(
+                                  user.name,
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 19,
+                                  ),
                                 ),
                               ),
-                            ),
-                            Text(
-                              '@${user.name}',
-                              style: const TextStyle(
-                                color: Pallete.greyColor,
-                                fontSize: 17,
+                              Text(
+                                '@${user.name} . ${timeago.format(
+                                  tweet.tweetedAt,
+                                  locale: 'en_short',
+                                )}',
+                                style: const TextStyle(
+                                  color: Pallete.greyColor,
+                                  fontSize: 17,
+                                ),
                               ),
-                            ),
-                          ],
-                        ),
-                      ],
+                            ],
+                          ),
+                          // replied to
+                          HashTagText(
+                            text: tweet.text,
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
