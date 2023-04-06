a9f262024086349bdadc1e730caef3bf301c282c
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-06 12:09:49 +0530

    show new tweet in realtime

--- lib/features/tweet/widgets/tweet_list.dart
+++ lib/features/tweet/widgets/tweet_list.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twitter_clone/common/common.dart';
+import 'package:twitter_clone/constants/constants.dart';
 import 'package:twitter_clone/features/tweet/controller/tweet_controller.dart';
 import 'package:twitter_clone/features/tweet/widgets/tweet_card.dart';
+import 'package:twitter_clone/models/tweet_model.dart';
 
 class TweetList extends ConsumerWidget {
   const TweetList({super.key});
@@ -11,14 +13,35 @@ class TweetList extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return ref.watch(getTweetsProvider).when(
           data: (tweets) {
-            print(tweets);
-            return ListView.builder(
-              itemCount: tweets.length,
-              itemBuilder: (BuildContext context, int index) {
-                final tweet = tweets[index];
-                return TweetCard(tweet: tweet);
-              },
-            );
+            return ref.watch(getLatestTweetProvider).when(
+                  data: (data) {
+                    if (data.events.contains(
+                      'databases.*.collections.${AppwriteConstants.tweetsCollection}.documents.*.create',
+                    )) {
+                      tweets.insert(0, Tweet.fromMap(data.payload));
+                    }
+
+                    return ListView.builder(
+                      itemCount: tweets.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        final tweet = tweets[index];
+                        return TweetCard(tweet: tweet);
+                      },
+                    );
+                  },
+                  error: (error, stackTrace) => ErrorText(
+                    error: error.toString(),
+                  ),
+                  loading: () {
+                    return ListView.builder(
+                      itemCount: tweets.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        final tweet = tweets[index];
+                        return TweetCard(tweet: tweet);
+                      },
+                    );
+                  },
+                );
           },
           error: (error, stackTrace) => ErrorText(
             error: error.toString(),
