48e9efeb6324743f24d10b6f8cb9d92e469be8d7
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-06 20:35:23 +0530

    added ExploreView

---
+++ lib/features/explore/view/explore_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ExploreView extends ConsumerStatefulWidget {
+  const ExploreView({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _ExploreViewState();
+}
+
+class _ExploreViewState extends ConsumerState<ExploreView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
