a457166e8451a3fda40f8929d96c78dafda1fd82
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-06 21:06:23 +0530

    added styling to search field

--- lib/features/explore/view/explore_view.dart
+++ lib/features/explore/view/explore_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:twitter_clone/theme/pallete.dart';
 
 class ExploreView extends ConsumerStatefulWidget {
   const ExploreView({super.key});
@@ -9,8 +10,42 @@ class ExploreView extends ConsumerStatefulWidget {
 }
 
 class _ExploreViewState extends ConsumerState<ExploreView> {
+  final searchController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    searchController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final appBarTextFieldBorder = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(50),
+      borderSide: const BorderSide(
+        color: Pallete.searchBarColor,
+      ),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: SizedBox(
+          height: 50,
+          child: TextField(
+            controller: searchController,
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.all(10).copyWith(
+                left: 20,
+              ),
+              fillColor: Pallete.searchBarColor,
+              filled: true,
+              enabledBorder: appBarTextFieldBorder,
+              focusedBorder: appBarTextFieldBorder,
+              hintText: 'Search Twitter',
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
