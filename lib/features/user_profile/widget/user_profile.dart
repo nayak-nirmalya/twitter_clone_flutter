abe84fbee36cac3a61dd0f24c7f79aec7b1d9988
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-07 11:52:43 +0530

    show follolw or edit btn on profile page

--- lib/features/user_profile/widget/user_profile.dart
+++ lib/features/user_profile/widget/user_profile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:twitter_clone/common/common.dart';
+import 'package:twitter_clone/features/auth/controller/auth_controller.dart';
 import 'package:twitter_clone/models/user_model.dart';
 import 'package:twitter_clone/theme/pallete.dart';
 
@@ -13,26 +15,66 @@ class UserProfile extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return NestedScrollView(
-      headerSliverBuilder: (context, innerBoxIsScrolled) {
-        return [
-          SliverAppBar(
-            expandedHeight: 150,
-            floating: true,
-            snap: true,
-            flexibleSpace: Stack(
-              children: [
-                Positioned.fill(
-                  child: userModel.bannerPic.isEmpty
-                      ? Container(color: Pallete.blueColor)
-                      : Image.network(userModel.bannerPic),
+    final currentUser = ref.watch(currentUserDetailsProvider).value;
+
+    return currentUser == null
+        ? const Loader()
+        : NestedScrollView(
+            headerSliverBuilder: (context, innerBoxIsScrolled) {
+              return [
+                SliverAppBar(
+                  expandedHeight: 150,
+                  floating: true,
+                  snap: true,
+                  flexibleSpace: Stack(
+                    children: [
+                      Positioned.fill(
+                        child: userModel.bannerPic.isEmpty
+                            ? Container(color: Pallete.blueColor)
+                            : Image.network(userModel.bannerPic),
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        child: CircleAvatar(
+                          backgroundImage: NetworkImage(
+                            userModel.profilePic,
+                          ),
+                          radius: 45,
+                        ),
+                      ),
+                      Container(
+                        alignment: Alignment.bottomRight,
+                        margin: const EdgeInsets.all(20),
+                        child: OutlinedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 25,
+                            ),
+                            side: const BorderSide(
+                              color: Pallete.whiteColor,
+                              width: 1.5,
+                            ),
+                          ),
+                          child: Text(
+                            currentUser.uid == userModel.uid
+                                ? "Edit Profile"
+                                : 'Follow',
+                            style: const TextStyle(
+                              color: Pallete.whiteColor,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ],
-            ),
-          ),
-        ];
-      },
-      body: Container(),
-    );
+              ];
+            },
+            body: Container(),
+          );
   }
 }
