cce389b2106fd7db5b835bdf583c5a18585215e9
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-08 14:14:20 +0530

    added api

--- lib/apis/notification_api.dart
+++ lib/apis/notification_api.dart
@@ -1,4 +1,5 @@
 import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:twitter_clone/constants/constants.dart';
@@ -9,19 +10,43 @@ import 'package:twitter_clone/models/notification_model.dart';
 final notificationAPIProvider = Provider((ref) {
   return NotificationAPI(
     db: ref.watch(appwriteDatabaseProvider),
+    realtime: ref.watch(appwriteRealtimeProvider),
   );
 });
 
 abstract class INotificationAPI {
   FutureEitherVoid createNotification(Notification notification);
+  Future<List<Document>> getNotifications(String uid);
+  Stream<RealtimeMessage> getLatestNotifications();
 }
 
 class NotificationAPI implements INotificationAPI {
   final Databases _db;
+  final Realtime _realtime;
 
-  NotificationAPI({
-    required Databases db,
-  }) : _db = db;
+  NotificationAPI({required Databases db, required Realtime realtime})
+      : _realtime = realtime,
+        _db = db;
+
+  @override
+  Future<List<Document>> getNotifications(String uid) async {
+    final docs = await _db.listDocuments(
+      databaseId: AppwriteConstants.databaseId,
+      collectionId: AppwriteConstants.notificationsCollection,
+      queries: [
+        Query.equal('uid', uid),
+      ],
+    );
+
+    return docs.documents;
+  }
+
+  @override
+  Stream<RealtimeMessage> getLatestNotifications() {
+    return _realtime.subscribe([
+      'databases.${AppwriteConstants.databaseId}.collections.${AppwriteConstants.notificationsCollection}.documents',
+    ]).stream;
+  }
 
   @override
   FutureEitherVoid createNotification(Notification notification) async {
